8be5b79915c552c8042466ba0c3df8a9a5dc9164
Edward Knueppel <edward.knueppel@zywave.com>
2022-02-03 08:59:57 -0600

    Feature test

--- yooxFunctionalFeatures.test.js
+++ yooxFunctionalFeatures.test.js
@@ -10,25 +10,10 @@ afterEach(async() => {
 
 test('Yoox features', async() => {
     await yooxPage.navigate()
-        // select "designer" from (top left toolbar) //
-    selectText = By.Xpath('//*[@id="sections-menu"]/li[2]/span')
-        // select "Burberry" from drop down //
-    selectText = By.Xpath('//*[@id="js-designerswomen"]/div[1]/div/div[1]/div[2]/div[5]/a')
-        // select "accessories", under "categories" //
-    selectText = By.Xpath('//*[@id="teleyooxCategories"]/div[2]/div/ul/li[1]/a')
-        // select "sizes" //
-    select.By.Xpath('//*[@id="teleyooxMysizes"]/div[1]/div[3]')
-        // select size "S" //
-    selectText = By.Xpath('//*[@id="listOfSizes"]/ul/li[3]/a')
-        // select a "color" //
-    selectText = By.Xpath('//*[@id="section-clr"]/div[1]/div[3]')
-        // select color "azure" //
-    selectText = By.Xpath('//*[@id="section-clr"]/div[2]/div/ul/li[1]/a/span[2]')
-        // select "material" //
-    selectText = By.Xpath('//*[@id="section-mtrl"]/div[1]/div[3]')
-        // select material "cotton"  //
-    selectText = By.Xpath('//*[@id="section-mtrl"]/div[2]/div/ul/li/a/span[2]')
 
+    await yooxPage.clickDesigners();
+
+    await yooxPage.clickBurberryDesigner();
 }, 30000)
 
 
--- yooxPage.js
+++ yooxPage.js
@@ -1,16 +1,18 @@
-const { By } = require("selenium-webdriver")
-const BasePage = require("./basePage")
+const { By } = require("selenium-webdriver");
+const BasePage = require("./basePage");
 
 module.exports = class YooxPage extends BasePage {
 
     // selectors //
 
-    searchIconBy = By.xpath('//*[@id="fayt-controls"]/svg/use')
-    searchInputBy = By.id('js-text-search-input')
-    newArrivalsBy = By.xpath('//*[@id="sections-menu"]/li[1]/span')
-    headerBy = By.xpath('//*[@id="logo"]/h1/a')
-    popUpBy = By.xpath('//*[@id="firstVisitChangeCountryLayer"]/div[1]/button')
+    searchIconBy = By.xpath('//*[@id="fayt-controls"]/svg/use');
+    searchInputBy = By.id('js-text-search-input');
+    newArrivalsBy = By.xpath('//*[@id="sections-menu"]/li[1]/span');
+    headerBy = By.xpath('//*[@id="logo"]/h1/a');
+    popUpBy = By.xpath('//*[@id="firstVisitChangeCountryLayer"]/div[1]/button');
     clothingBy = By.xpath('//*[@id="js-newarrivalswomen"]/div[1]/div/div[1]/div/div[1]/a');
+    designersBy = By.Xpath('//*[@id="sections-menu"]/li[2]/span');
+    burberryDesignerBy = By.Xpath('//*[@id="js-designerswomen"]/div[1]/div/div[1]/div[2]/div[5]/a')
 
     constructor(driver, url) {
         super(driver, url)
@@ -20,19 +22,19 @@ module.exports = class YooxPage extends BasePage {
     // methods //
 
     async getYooxHomeHeaderText() {
-        return await this.getText(this.headerBy)
+        return await this.getText(this.headerBy);
     }
 
     async clickNewArrivals() {
-        await this.click(this.newArrivalsBy)
+        await this.click(this.newArrivalsBy);
     }
 
     async dismissChangeCountryPopUp() {
-        await this.click(this.popUpBy)
+        await this.click(this.popUpBy);
     }
 
     async clickSearchIcon() {
-        await this.click(this.searchIconBy)
+        await this.click(this.searchIconBy);
     }
 
     async searchOn(term) {
@@ -41,5 +43,13 @@ module.exports = class YooxPage extends BasePage {
 
     async clickClothing() {
         return await this.click(this.clothingBy);
+    }
+
+    async clickDesigners() {
+        return await this.click(this.designersBy);
+    }
+
+    async clickBurberryDesigner() {
+        return await this.click(this.burberryDesignerBy)
     }
 }
